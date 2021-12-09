3a938b6f2f69e031ffe1c8a91cffe495d9042e58
rahulram1996 <Rahul.ram@imaginate.in>
2021-12-09 17:31:59 +0530

    chat manager introduced

---
+++ src/ChatManager.cs
@@ -0,0 +1,77 @@
+//using ExitGames.Client.Photon;
+//using Photon.Chat;
+//using Photon.Pun;
+using Photon.Pun;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChatManager : MonoBehaviour//, IChatClientListener
+{
+   // ChatClient chatClient;
+    [SerializeField] private string UserId;
+    [SerializeField] private string channelName;
+
+    [SerializeField] private bool OnChatConnected = false;
+    public PhotonView view;
+    public ChatUIController chatUIController;
+
+    private void OnEnable()
+    {
+        view = gameObject.GetComponent<PhotonView>();
+#if UNITY_STANDALONE_WIN || MAC
+        chatUIController = FindObjectOfType<ChatUIController>();
+        if (view.IsMine)
+        {
+            chatUIController.OnsetChatManager(this);
+        }
+#endif
+    }
+
+    public void OnSendMessage(string sender, string message)
+    {
+        object[] data = {sender, message};
+        view.RPC("publicMessageReceived", RpcTarget.AllViaServer , data);
+    }
+
+    [PunRPC]
+    private void publicMessageReceived(object[] data)
+    {
+#if UNITY_STANDALONE_WIN || MAC
+        object[] val = data;
+        string sender = (string) val[0];
+        string message = (string) val[1];
+        chatUIController.OnMessageReceived(sender, message);
+#endif
+    }
+    /*
+        public void SetUserName(string value)
+        {
+            UserId = value;
+        }
+
+        public void SetChannelName(string value)
+        {
+            channelName = value;
+        }
+
+            public void ConnectToChat()
+            {
+        #if UNITY_STANDALONE_WIN || MAC
+                chatClient = new ChatClient(this);
+                chatClient.Connect(PhotonNetwork.PhotonServerSettings.AppSettings.AppIdChat, PhotonNetwork.AppVersion, new AuthenticationValues(UserId));
+        #endif
+            }
+
+        private void Update()
+        {
+
+            #if UNITY_STANDALONE_WIN || MAC
+                    if(chatClient!=null)
+                    chatClient.Service();
+            #endif
+
+        }
+        */
+}
